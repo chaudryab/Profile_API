5eeb6c4d9f5f69fd3187c11af60034428a2ae9cf
chaudryab <chaudryab@gmail.com>
2021-06-26 15:24:24 +0500

    commit

--- howgh/settings.py
+++ howgh/settings.py
@@ -113,8 +113,8 @@ DATABASES = {
         'HOST': 'ec2-54-90-211-192.compute-1.amazonaws.com',
         'PORT': '5432'
 
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
